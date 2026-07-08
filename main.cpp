6928e35cf3e2174b982e8049a652fa9ca52f4936
unknow <unknow@unknow.unknow>
2026-07-08 13:49:00 -0400

    feat: feat function Bunner Show Info

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include "FileM.hpp"
+
+void    Bunner(void) {
+    std::cout << "-----------------------------------------------" << std::endl;
+    std::cout << "|                                             |" << std::endl;
+    std::cout << "|          Welcome to File Replace            |" << std::endl;
+    std::cout << "|                                             |" << std::endl;
+    std::cout << "-----------------------------------------------" << std::endl;
+}
+
+int main(void) {
+
+    std::string fileName;
+    std::string s1;
+    std::string s2;
+
+    Bunner();
+    getline(std::cin, fileName);
+    getline(std::cin, s1);
+    getline(std::cin, s2);
+
+    FileM *newFile = new FileM((char *)fileName.c_str());
+
+    if (fileName.empty() || s1.empty() || s2.empty()) return (std::cout << "Invalid args", 1);
+    
+    newFile->createFile((char *)s1.c_str(), (char *)s2.c_str());
+    
+    newFile->showOriginFile();
+    std:: cout << "\n" << std::endl;
+
+    if (newFile) delete newFile;
+    return (0);
+}
